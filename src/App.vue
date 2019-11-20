940e95c78b6a0c0239996a7ac040f61e95e20aab
dustyndoyle <ddoyle512@gmail.com>
2019-11-19 21:42:17 -0500

    Create Todo Frontend with Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>What's Todo?</h1>
+    <input type="text" placeholder="What would you like todo?" v-on:keyup.enter="addTodo" />
+    <ul>
+      <TodoItem v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.id" v-on:remove-todo="removeTodo"></TodoItem>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      todos: [
+        { name: 'Todo 1', isComplete: false, id: Date.now() },
+        { name: 'Todo 2', isComplete: false, id: Date.now() + 1 }
+      ]
+    }
+  },
+  methods: {
+    addTodo(e) {
+      let todo = e.target.value;
+
+      this.todos.push({ name: todo, isComplete: false, id: Date.now() });
+      e.target.value = '';
+    },
+    removeTodo(id) {
+      this.todos = this.todos.filter( todo => todo.id !== id );
+    }
+  },
   components: {
-    HelloWorld
+    TodoItem
   }
 }
 </script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li>
+        <label><input type="checkbox" v-model="todo.isComplete" /></label>
+        <span>&nbsp;</span>
+        <del v-if="todo.isComplete">{{todo.name}}</del>
+        <span v-else>{{todo.name}}</span>
+        <span>&nbsp;</span>
+        <button type="button" v-on:click="$emit( 'remove-todo', todo.id )">X</button>
+      </li>
+</template>
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    todo: Object
+  }
+}
+</script>
